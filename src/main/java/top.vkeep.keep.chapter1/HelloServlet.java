e13e9e7973adc22161076bc0fd01b4d464ec2166
zhourl <zhouronglv@gmail.com>
2018-07-01 09:57:39 +0800

    Init

---
+++ src/main/java/top.vkeep.keep.chapter1/HelloServlet.java
@@ -0,0 +1,30 @@
+package top.vkeep.keep.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * <p>@author: zhourl(zhouronglv@gmail.com)
+ * <p>@description: keep
+ * <p>@version: v1.0
+ * <p>@date: 2018/6/30
+ **/
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = sdf.format(new Date());
+        System.out.println("currentTime:" + currentTime);
+        req.setAttribute("currentTime", currentTime);
+
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhourl
+  Date: 2018/6/30
+  Time: 10:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello Servlet</title>
+</head>
+<body>
+    <h1>Hello!</h1>
+    <h2>当前时间：${currentTime}</h2>
+</body>
+</html>
