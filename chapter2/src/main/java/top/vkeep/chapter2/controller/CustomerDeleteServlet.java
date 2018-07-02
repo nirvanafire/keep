de59b22e5bf8b305b243b9a7384d053c8030f347
zhourl <zhouronglv@gmail.com>
2018-07-02 23:25:49 +0800

    初始化Servlet

---
+++ chapter2/src/main/java/top/vkeep/chapter2/controller/CustomerDeleteServlet.java
@@ -0,0 +1,14 @@
+package top.vkeep.chapter2.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * <p>@author: zhourl(zhouronglv@gmail.com)
+ * <p>@description: keep
+ * <p>@version: v1.0
+ * <p>@date: 2018/7/2
+ **/
+@WebServlet("/customer_delete")
+public class CustomerDeleteServlet extends HttpServlet {
+}
--- chapter2/src/main/java/top/vkeep/chapter2/controller/CustomerServlet.java
+++ chapter2/src/main/java/top/vkeep/chapter2/controller/CustomerServlet.java
@@ -1,32 +1,31 @@
 package top.vkeep.chapter2.controller;
 
+import top.vkeep.chapter2.model.Customer;
+import top.vkeep.chapter2.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
- * 创建客户
  * <p>@author: zhourl(zhouronglv@gmail.com)
  * <p>@description: keep
  * <p>@version: v1.0
- * <p>@date: 2018/7/1
+ * <p>@date: 2018/7/2
  **/
-@WebServlet("/customer_create")
-public class CustomerCreateServlet extends HttpServlet {
-    /**
-     * 进入 创建客户 界面
-     */
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/view/customer_create.jsp").forward(req, resp);
+        List<Customer> customerList = new CustomerService().getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
     }
 
-    /**
-     * 处理 创建客户 请求
-     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
