554226e5245153a9670c6faeb7eb6d579b9a05ea
zhourl <zhouronglv@gmail.com>
2018-07-17 13:31:38 +0800

    test:shiro demo

---
+++ chapter5/src/main/java/top/vkeep/chapter5/entity/User.java
@@ -0,0 +1,35 @@
+package top.vkeep.chapter5.entity;
+
+/**
+ * <p>@author: zhourl(zhouronglv@gmail.com)
+ * <p>@description: keep
+ * <p>@since: v1.1
+ * <p>@date: 2018-07-17
+ **/
+public class User {
+
+    private String username;
+
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
