d11ec8776876aec9b82fae1ce91e836eafff8b86
zhourl <zhouronglv@gmail.com>
2018-07-18 08:31:39 +0800

    refactor:security plugin

---
+++ smart-plugin-security/src/main/java/top/vkeep/smart/plugin/security/SecurityHelper.java
@@ -0,0 +1,19 @@
+package top.vkeep.smart.plugin.security;
+
+/**
+ * Security助手类
+ *
+ * <p>@author: Zhourl(zhouronglv@gmail.com)
+ * <p>@description: keep
+ * <p>@version: v1.0
+ * <p>@date: 2018-07-17
+ **/
+public class SecurityHelper {
+    public static void login(String username, String password) {
+
+    }
+
+    public static void logout() {
+
+    }
+}
